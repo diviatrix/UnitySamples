3824053b180d728e8d718a90d59a2b9ceaf12e89
Unknown <diviatrix@gmail.com>
2018-07-20 18:02:06 +0300

    second implementation on tileMatching
    Only beginning. reading textures and generation

--- TileMatching/Assets/Scripts/Tile.cs
+++ TileMatching/Assets/Scripts/Tile.cs
@@ -2,8 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class Tile : MonoBehaviour {
 
-	public bool isOpen = false;
 	public bool isDeath = false;
+	public Sprite back,front;	
+
+	// bool which changes texture of current sprite according to status
+	// set private one to store it inside
+	private bool _isOpen;
+
+	public bool isOpen
+	{
+		get { return _isOpen; }
+
+		set
+		{
+			_isOpen = value;
+			
+			if (_isOpen == true)
+			{
+				Debug.Log("Set true");
+
+				GetComponent<SpriteRenderer>().sprite = front;
+			}
+			else 
+			{
+				Debug.Log("Set False");
+				GetComponent<SpriteRenderer>().sprite = back;
+			}
+		}
+	}
+
+	// switch status of tile
+	public void SwitchOpen()
+	{
+		isOpen = !isOpen;
+	}
+
+	void Start()
+	{
+
+	}
+	
 }

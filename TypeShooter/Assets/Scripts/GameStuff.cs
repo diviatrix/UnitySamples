475379bb8f55b5c24c981d6c6bdad35b5a5c12f8
Unknown <diviatrix@gmail.com>
2018-07-26 13:59:05 +0300

    added TypeShooter project

---
+++ TypeShooter/Assets/Scripts/GameStuff.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+public class GameStuff : MonoBehaviour {
+	
+	String[] allWords = new String[500000];
+
+	public float cooldown;
+	float lastWordTime;
+	public Text wordTextUI;
+	public GameObject textPrefab;
+	public List<GameObject> wordsInGame = new List<GameObject>();
+
+	System.Random rnd = new System.Random();
+
+	// Use this for initialization
+	void Start () 
+	{
+		loadWords();
+		lastWordTime = Time.time;		
+	}
+
+	// cycle for word spawning
+	void SpawnWords()
+	{	
+		if ((lastWordTime + cooldown) > Time.time ) return;
+
+		if (wordsInGame.Count < 10)
+		{
+			lastWordTime = Time.time;
+			GameObject go = GameObject.Instantiate(textPrefab);
+
+			string word = randomWord();
+			
+			go.transform.position = new Vector3(-10, UnityEngine.Random.Range(-3,5), -1);			
+			go.GetComponent<TextMesh>().text = word;
+			go.transform.name = word;
+			Debug.Log(word.Length);	
+			go.GetComponent<WordBehavior>().speed = 1/(float)word.Length;
+
+			wordsInGame.Add(go);
+		}
+	}
+
+
+	void FixedUpdate()
+	{
+		// this
+		SpawnWords();
+
+		// remove null elements from wordsingame list
+		wordsInGame.Remove(null);
+	}
+
+
+	// load dictionary from file
+	void loadWords()
+	{
+		string line; 
+		// Open the text file using a stream reader.
+        using (StreamReader sr = new StreamReader("Assets/Resources/Text/words_ru.txt"))
+        {
+			int i = 0;
+	    // Read the stream to a string, and write the string to the console.
+            while((line = sr.ReadLine()) != null)  
+			{  
+				//Debug.Log(line);  
+				allWords[i] = line;
+				i++;
+			}
+			
+			String[] tempArray = new String[i];
+
+			for(int a = 0; a <i; a++)
+			{
+				tempArray[a] = allWords[a];
+			}
+			allWords = tempArray;
+
+			Debug.Log(allWords.Length);
+        }			
+	}
+
+	// gives random word from dictionary
+	string randomWord() 
+	{	
+		int id = rnd.Next(0,allWords.Length);
+		return(allWords[id]);
+	}
+
+	// run every frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ TypeShooter/Assets/Scripts/WordBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordBehavior : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.AddComponent<BoxCollider>();
+		//speed = speed/100 * (GetComponent<TextMesh>().text.Length);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate(new Vector3(speed/10,0,0));
+		if(transform.position.x >= 9){
+			Destroy(gameObject);
+		}
+	}
+}
